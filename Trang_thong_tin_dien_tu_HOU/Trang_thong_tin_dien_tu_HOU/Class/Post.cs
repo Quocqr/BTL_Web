9080d2ce86f0e233d01a0d2ef278b7da32543f53
Quocqr <nguyenhuyquoc03022005@gmail.com>
2024-10-31 22:43:07 +0700

    them
    11

---
+++ Trang_thong_tin_dien_tu_HOU/Trang_thong_tin_dien_tu_HOU/Class/Post.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Trang_thong_tin_dien_tu_HOU
+{
+    public class Post
+    {
+        public string Title { get; set; }
+        public string Content { get; set; }
+
+        public Post(string title, string content)
+        {
+            this.Title = title;
+            this.Content = content;
+        }
+    }
+}
---
+++ Trang_thong_tin_dien_tu_HOU/Trang_thong_tin_dien_tu_HOU/Class/User.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Trang_thong_tin_dien_tu_HOU
+{
+    public class User
+    {
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public object Repeater1 { get; private set; }
+
+        public User(string username, string password)
+        {
+            Username = username;
+            Password = password;
+            Repeater1 = Repeater1;
+        }
+    }
+}
